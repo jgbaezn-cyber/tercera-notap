7d41adeb9090bdb242274e6ceff9366107aa4bfa
jgbaezn-cyber <jgbaezn@ufpso.edu.co>
2025-09-05 21:30:06 -0500

    unidad - uno

--- src/AsignacionVariables.java
+++ src/AsignacionVariables.java
@@ -2,10 +2,10 @@ public class AsignacionVariables {
     public static void main(String[] args) throws Exception {
         int edad = 17;
         double altura = 1.65;
-        String nombre = "Jerix";
+        char iniNombre = 'J';
         
         System.out.println("=== INFORMACIÓN DE LA PERSONA ===");
-        System.out.println("Nombre: " + nombre);
+        System.out.println("Inicial del nombre: " + iniNombre);
         System.out.println("Edad: " + edad + " años");
         System.out.println("Altura: " + altura + " metros");
         System.out.println("¿Es mayor de edad? " + (edad >= 18));
